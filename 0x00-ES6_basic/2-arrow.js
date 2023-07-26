c32d16e2997af64c6f96493647a9d9a6e2094c67
rukkyme <communicatejoy@yhaoo.com>
2023-07-26 18:40:55 +0100

    arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,9 @@
 export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    this.addNeighborhood = (newNeighborhood) => {
-      this.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return this.sanFranciscoNeighborhoods;
-    };
-  }
-  
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  // using arrow function to define the addNeigborhood function
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
