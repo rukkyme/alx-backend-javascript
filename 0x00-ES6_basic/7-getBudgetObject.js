cc922dc6c880dca4bd4bb5c1549c3f80467b4142
rukkyme <communicatejoy@yhaoo.com>
2023-07-26 19:02:28 +0100

    basics

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,10 @@
+export default function getBudgetObject(income, gdp, capita) {
+  // Use shorthand property syntax to create the budget object
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
