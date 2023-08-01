18b155ff8aa461065566e2ce6b66cbaeb33e9d5d
rukkyme <communicatejoy@yhaoo.com>
2023-08-01 22:21:53 +0100

    array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+export default function getListStudents() {
+  const arrayOfStudents = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return arrayOfStudents;
+}
+
+const students = getListStudents();
+console.log(students);
