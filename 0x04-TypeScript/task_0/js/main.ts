3696db3b5f8ab9d307f8b56fda160c6e9235a564
rukkyme <communicatejoy@yhaoo.com>
2023-08-02 15:35:44 +0100

    typscript

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,42 +1,44 @@
-//Step 1: Define the interface
 interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
-    location: string;
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string
 }
 
-    // Step 2: Create two student objects
-const student1: Student = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-    location: "New York"
-};
-
-const student2: Student = {
-    firstName: "Jane",
-    lastName: "Smith",
-    age: 23,
-    location: "Los Angeles"
-};
-
-    // Step 3: Create the array studentsList
-const studentsList: Student[] = [student1, student2];
-
-    // Step 4: Render the table
-const tableBody = document.querySelector("#studentTable tbody");
-
-studentsList.forEach((student) => {
-    const row = document.createElement("tr");
-    const firstNameCell = document.createElement("td");
-    const locationCell = document.createElement("td");
-
-    firstNameCell.textContent = student.firstName;
-    locationCell.textContent = student.location;
-
-    row.appendChild(firstNameCell);
-    row.appendChild(locationCell);
-
-    tableBody.appendChild(row);
+const firstStudent : Student ={
+    firstName: 'James',
+    lastName: 'Bond',
+    age: 22,
+    location: 'Nigeria'
 }
+
+const secondStudent : Student = {
+    firstName: 'Taiwo',
+    lastName: 'Samuel',
+    age: 20,
+    location: 'Nigeria'
+}
+
+const studentsList: Student[] = [firstStudent, secondStudent];
+
+
+function renderTable() {
+    const table = document.createElement('table');
+  
+    studentsList.forEach((student) => {
+      const row = document.createElement('tr');
+      const firstNameCell = document.createElement('td');
+      const locationCell = document.createElement('td');
+  
+      firstNameCell.textContent = student.firstName;
+      locationCell.textContent = student.location;
+  
+      row.appendChild(firstNameCell);
+      row.appendChild(locationCell);
+  
+      table.appendChild(row);
+    });
+    document.body.appendChild(table);
+}
+
+renderTable();
